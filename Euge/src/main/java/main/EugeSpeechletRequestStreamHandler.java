c981e6570105cce4ce5c66a0e1ad8f473180bd85
bfisher82 <bfisher@ebfour.com>
2017-03-15 13:54:55 +0000

    First commit

---
+++ Euge/src/main/java/main/EugeSpeechletRequestStreamHandler.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+/**
+ * 
+ *
+ */
+public class EugeSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+	private static final Set<String> supportedApplicationIds = new HashSet<String>();
+
+	static {
+		//	skill id
+		supportedApplicationIds.add("INSERT AMAZON SKILL ID HERE");
+
+	}
+
+	public EugeSpeechletRequestStreamHandler() {
+		super(new EugeSpeechlet(), supportedApplicationIds);
+	}
+}
